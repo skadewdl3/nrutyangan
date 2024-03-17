ca4264e6f46c446bb9db5a033a8729ac02678f55
Soham Karandikar <sohamk10@gmail.com>
2024-03-17 18:04:08 +0530

    Added timeline effect to our story page

---
+++ components/Circle.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps({
+    top: {
+        required: true,
+        type: Number
+    },
+    size: {
+        required: false,
+        type: Number,
+        default: 60
+    },
+    color: {
+        required: false,
+        type: String,
+        default: '#ffffff'
+    },
+    borderColor: {
+        required: false,
+        type: String,
+        default: '#283C27'
+    },
+    borderSize: {
+        required: false,
+        type: Number,
+        default: 10
+    }
+})
+</script>
+
+<template>
+    <div class="circle__wrapper absolute left-1/2 -translate-x-1/2 -translate-y-1/2" :style="{ top: `${props.top}px`, width: `${props.size}px` }">
+    <div
+        data-aos="zoom-in"
+      class="timeline__circle relative w-full rounded-full border-solid aspect-square"
+      :style="{background: props.color, borderColor: props.borderColor, borderWidth: `${props.borderSize}px`}"
+    >
+    </div>
+    </div>
+</template>
---
+++ components/Line.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+    start: {
+        required: true,
+        type: Number
+    },
+    end: {
+        required: true,
+        type: Number
+    },
+    color: {
+        required: false,
+        type: String,
+        default: '#283C27'
+    },
+    animate: {
+        required: false,
+        type: Boolean,
+        default: true
+    }
+})
+
+console.log(props.start, props.end);
+
+</script>
+
+<template>
+    <div class="line__wrapper absolute left-1/2 -translate-x-1/2 w-4" :style="{top: `${props.start}px`, height: `${props.end - props.start}px`}">
+        <div :data-aos="props.animate ? 'slide-down' : 'zoom-in-down'" class="timeline__line w-full h-full  rounded-lg" :style="{background: props.color}"></div>
+    </div>
+</template>
