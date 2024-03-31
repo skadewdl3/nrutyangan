dd56cfeda514b56f0fc222fa40065ebf38fc6a87
Soham Karandikar <sohamk10@gmail.com>
2024-03-31 16:24:11 +0530

    Switched to pnpm. Added flyout menu

---
+++ components/FlyoutMenu.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+	<div class="absolute top-0 right-0 w-3/4 h-screen z-[11] bg-black/85">
+		<div class="wrapper w-full h-full relative">
+			<button @click="emit('close')" class="text-white text-2xl cursor-pointer absolute top-4 right-16"><Icon name="charm:menu-hamburger" /></button>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style lang="stylus">
+
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,17 +21,43 @@ const items = [
     link: '#join'
   }
 ]
+
+const flyout = ref(false)
+const temp = () => {
+  flyout.value = !flyout.value
+  console.log('this ran', flyout.value);
+  
+}
 </script>
 
 <template>
       <div class="navbar z-10 sticky top-0 left-0 bg-primary text-white px-16 py-4 flex items-center justify-between shadow-black/50 shadow-2xl">
-    <h1 class="navbar__left text-2xl cursor-pointer" @click="navigateTo('/')">
+    <h1 class="navbar__left md:text-xl lg:text-2xl cursor-pointer" @click="navigateTo('/')">
       Nrutyangan - Kathak Dance Academy
     </h1>
-    <div class="navbar__right">
+    <div class="navbar__right hidden lg:block">
       <a v-for="{name, link} in items" class="ml-4 drop-shadow-md cursor-pointer" :href="link">{{ name }}</a>
+    </div>
+    <Teleport to="body">
+      <Transition name="fade">
+        <FlyoutMenu v-if="flyout" @close="flyout = false">
+          <a v-for="{name, link} in items" class="ml-4 drop-shadow-md cursor-pointer" :href="link">{{ name }}</a>
+        </FlyoutMenu>
+      </Transition>
+    </Teleport>
+    <div class="navbar__right lg:hidden">
+      <button class="text-white text-2xl cursor-pointer" @click="flyout = true"><Icon name="charm:menu-hamburger" /></button>
     </div>
   </div>
 </template>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+.fade-enter-active,
+.fade-leave-active 
+  transition transform 0.5s ease
+
+.fade-enter-from,
+.fade-leave-to 
+  transform translateX(100%)
+
+</style>
