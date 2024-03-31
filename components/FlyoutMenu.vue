8416b677625834ae7887c8194fb1f75c96bb874f
Soham Karandikar <sohamk10@gmail.com>
2024-03-31 16:58:51 +0530

    Cimpleted flyout meu

--- components/FlyoutMenu.vue
+++ components/FlyoutMenu.vue
@@ -1,12 +1,39 @@
 <script setup lang="ts">
 const emit = defineEmits(['close'])
+const props = defineProps({
+	items: {
+		type: Array<{name: String, link: String}>,
+		required: true
+	}
+})
+const shown = ref(false)
+
+onMounted(() => {
+	setTimeout(() => {
+		shown.value = true
+	}, 500)
+	
+	console.log(props.items);
+})
+
+const close = () => {
+	emit('close')
+}
 </script>
 
 <template>
-	<div class="absolute top-0 right-0 w-3/4 h-screen z-[11] bg-black/85">
-		<div class="wrapper w-full h-full relative">
-			<button @click="emit('close')" class="text-white text-2xl cursor-pointer absolute top-4 right-16"><Icon name="charm:menu-hamburger" /></button>
-			<slot />
+	<div class="absolute top-0 right-0 w-full h-screen z-[11] flex justify-end" @click="close">
+		<div class="wrapper w-[90%] lg:w-3/4 h-full relative bg-black/85 text-white text-2xl" @click.stop="">
+
+			<div class="close-btn w-full px-4 lg:px-16 py-4 flex justify-end">
+				<button @click="close" class="text-white transition-all text-2xl cursor-pointer" :class="{'scale-100': shown, 'scale-0': !shown}"><Icon name="material-symbols-light:close-rounded"/></button>
+			</div>
+			<div class="flex flex-col items-start justify-center">
+				<div class="flyout-item w-full" v-for="(item, i) in props.items">
+					<slot :content="item.name" :link="item.link.toString()"  />
+					<div class="w-full my-2 h-0.5 bg-white" v-if="i != props.items.length - 1"></div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -31,7 +31,7 @@ const temp = () => {
 </script>
 
 <template>
-      <div class="navbar z-10 sticky top-0 left-0 bg-primary text-white px-16 py-4 flex items-center justify-between shadow-black/50 shadow-2xl">
+      <div class="navbar z-10 sticky top-0 left-0 bg-primary text-white px-4 lg:px-16 py-4 flex items-center justify-between shadow-black/50 shadow-2xl">
     <h1 class="navbar__left md:text-xl lg:text-2xl cursor-pointer" @click="navigateTo('/')">
       Nrutyangan - Kathak Dance Academy
     </h1>
@@ -40,14 +40,19 @@ const temp = () => {
     </div>
     <Teleport to="body">
       <Transition name="fade">
-        <FlyoutMenu v-if="flyout" @close="flyout = false">
-          <a v-for="{name, link} in items" class="ml-4 drop-shadow-md cursor-pointer" :href="link">{{ name }}</a>
+        <FlyoutMenu v-if="flyout" @close="flyout = false" :items="items">
+          <template #default="item">
+            <a class="drop-shadow-md cursor-pointer px-8 my-[100rem]" :href="item.link">{{ item.content }}</a>
+
+          </template>
         </FlyoutMenu>
       </Transition>
     </Teleport>
-    <div class="navbar__right lg:hidden">
-      <button class="text-white text-2xl cursor-pointer" @click="flyout = true"><Icon name="charm:menu-hamburger" /></button>
-    </div>
+    <Transition name="scale-down">
+      <div class="navbar__right lg:hidden" v-if="!flyout">
+        <button class="text-white text-2xl cursor-pointer" @click="flyout = true"><Icon name="charm:menu-hamburger" /></button>
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -59,5 +64,13 @@ const temp = () => {
 .fade-enter-from,
 .fade-leave-to 
   transform translateX(100%)
+
+.scale-down-enter-active,
+.scale-down-leave-active 
+  transition transform 0.5s ease
+
+.scale-down-enter-from,
+.scale-down-leave-to 
+  transform scale(0)
 
 </style>
